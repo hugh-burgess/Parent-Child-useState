dfd2480e1609cf36c489270bfea880a8b3cbcc4e
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-05 14:42:43 +0200

    Initial Commit Setup

---
+++ src/Child.js
@@ -0,0 +1,7 @@
+export default function Child() {
+  return (
+    <div className="child">
+      <h1 className="h1Child">Child</h1>
+    </div>
+  );
+}
---
+++ src/Parent.js
@@ -0,0 +1,10 @@
+import Child from "./Child";
+
+export default function Parent() {
+  return (
+    <div className="parent">
+      <h1 className="h1Parent">Parent</h1>
+      <Child />
+    </div>
+  );
+}
